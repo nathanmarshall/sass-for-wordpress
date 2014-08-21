c6abfb6c5f83419fa205123e241ffe3df5898315
Nathan Marshall <Nathan@Admins-iMac.local>
2014-08-21 16:16:55 -0700

    First commit

---
+++ GruntFile.js
@@ -0,0 +1,81 @@
+/*! 
+ * Wordpress Starter Template w/Sass
+ * http://powershifter.com
+ * @auhtor Nathan Marshall at Poweshifter 
+ */
+
+'use strict';
+
+/** 
+ * Grunt Module 
+ */
+
+module.exports = function(grunt) {
+
+	/**
+	 * Grunt Configuration
+	 */
+
+	 grunt.initConfig({
+	 	
+	 	pkg: grunt.file.readJSON('package.json'),
+	 	project: {
+	 		css: [
+	 			'sass/stylesheet.scss'
+	 		]
+	 	},
+
+	 	/**
+	 	 * Sass
+	 	 */
+	 	sass: {
+	 		dev: {
+	 			options: {
+	 				style: 'expanded',
+	 				compass: false,
+	 				sourcemap: 'none'
+	 			},
+	 			files: {
+	 				'style.css' : '<%= project.css %>'
+	 			}
+	 		},
+	 		dist: {
+	 			options: {
+	 				style: 'compressed',
+	 				compass: false,
+	 				sourcemap: 'none'
+	 			},
+	 			files: {
+	 				'style.css' : '<%= project.css %>'
+	 			}
+	 		}
+	 	},
+
+	 	/**
+	 	 * Watch
+	 	 * polls for changes to .scss files.
+	 	 */
+
+	 	 watch: {
+	 	 	sass: {
+	 	 		files: 'sass/{,*/}*.{scss, sass}',
+	 	 		tasks: ['sass:dev']
+	 	 	}
+	 	 }
+	 });
+
+	/**
+	 * Load Grunt plugins
+	 */
+
+	require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+	 /**
+	  * Default task
+	  * Run `grunt` on the command line
+	  */
+	 grunt.registerTask('default', [
+	   'sass:dev',
+	   'watch'
+	 ]);
+};
